60be9b818d5c50706997de09e94a843c0ac06824
Jeremiah Nwaeze <nwaezejerry@gmail.com>
2020-03-09 20:20:57 +0100

    updating repo

---
+++ src/main/java/com/example/javabackend/dao/PersonDataAccessService.java
@@ -0,0 +1,55 @@
+package com.example.javabackend.dao;
+
+import com.example.javabackend.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository("postgres")
+public class PersonDataAccessService implements PersonDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        final String sql = "SELECT id, name FROM person WHERE id = ?";
+        Person person = jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultSet -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
+        return Optional.ofNullable(person);
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/example/javabackend/dao/PersonDataAccessServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.javabackend.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonDataAccessServiceTest {
+
+    @Test
+    void selectAllPeople() {
+    }
+}
